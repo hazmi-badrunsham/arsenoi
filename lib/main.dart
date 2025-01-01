dabf73d243dc93564122984e7b782fc11765dc0f
YourName <badrunsham.hazmi@gmail.com>
2025-01-02 01:31:26 +0800

    hihi

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Loading Screen',
+      title: 'Loading Screen',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
